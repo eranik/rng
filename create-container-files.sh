cd7adce0a78d926511cdaadb47a4245605c27d2d
a <a@a.a>
2020-07-09 02:51:09 +0000

    add script for gathering minimal binaries

---
+++ create-container-files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf ./rng-container
+mkdir -p ./rng-container
+cd ./rng-container
+mkdir -p ./bin
+cp ../rng ./bin/
+for i in `ldd ./bin/rng | grep -v linux-vdso.so.1 | awk {' if ( $3 == "") print $1; else print $3 '}`; do cp --parents $i ./ ; done
+cp ../config.json .
+cd ..
+tar -czvf ./rng-container.tar.gz ./rng-container -C rng-container
+
+# docker import ./ping-container.tar.gz ping-container
+
+
